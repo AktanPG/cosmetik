d261b9a22d13fc6931f0e9be2933e9e434c7fd3e
Aktan <thekambipg@gmail.com>
2018-12-05 04:15:24 -1000

    2 section added

--- server.js
+++ server.js
@@ -33,7 +33,35 @@ app.use('/', (req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        products: [
+            {
+                title: "New collection pamada",
+                images: ['https://pics.ru/wp-content/uploads/2016/06/gloss4.jpg'],
+                cost: "399"
+            },
+            {
+                title: "Biotherm - Blue Therapy Red",
+                images: ['https://cosmeticall.com.ua/store/pub/media/catalog/product/cache/image/700x560/5eabe8d3362c0fa2bcd1ca09df4e84f2/c/u/cua22078676701.jpg'],
+                cost: "699"
+            },
+            {
+                title: "Tush from israel",
+                images: ['https://venalia.ru/media/catalog/product/cache/1/small_image/516x490/9df78eab33525d08d6e5fb8d27136e95/s/c/sch01-seacare-spa-_mineral-mud-shampoo-for-all-hair-types-400-ml__1.jpg'],
+                cost: "120"
+            },
+            {
+                title: "Parfem",
+                images: ['https://kosmet.by/files/products/127_1.600x600.jpg?da2adafcc569be76e783a25ddb3d94b0'],
+                cost: "2499"
+            },
+            {
+                title: "Shapmoo for kids",
+                images: ['http://www.nsp-business.com.ua/i/bremani/care1.jpg'],
+                cost: "799"
+            }
+        ]
+    });
 });
 
 app.use('/', require('./routes/auth'));
